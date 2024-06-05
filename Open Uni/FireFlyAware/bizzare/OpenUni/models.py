12ac2f2be0601630ecee6573d951d74ba0578eeb
StrainedPUFF <168001964+StrainedPUFF@users.noreply.github.com>
2024-06-05 10:19:56 +0300

    Miscelleneous

--- Open Uni/FireFlyAware/bizzare/OpenUni/models.py
+++ Open Uni/FireFlyAware/bizzare/OpenUni/models.py
@@ -1,12 +1,45 @@
-from django.db import models
-import datetime
+# import datetime
+# from django.utils import timezone
+# from django.db import models
+
+# # Create your models here.
+# class Question (models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+#     def __str__(self):
+#         return self.question_text
+#     def was_published_recently(self):
+#         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+#     class Choice (models.Model):
+#         question = models.ForeignKey(Question , on_delete=models.CASCADE)
+#         answer_text =models.CharField(max = 200)
+#         votes = models.IntegerField(default=0)
+        
+        
+#         def __str__(self):
+#             return self.choice_text
+        
+import datetime 
 from django.utils import timezone
 
 # Create your models here.
+from django.db import models
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
+    pub_date = models.DateTimeField("date published")
+    
+def __str__(self):
         return self.question_text
-    def was_published_recently(self):
+def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.answer_text
+    
